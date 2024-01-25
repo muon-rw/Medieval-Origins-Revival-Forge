2bc3645758a674fdacf98e8fd5ea75f67a62bf73
muon-rw <ravenmenville@gmail.com>
2024-01-24 20:18:46 -0600

    is_undead, harvestable_crops conditions

--- src/main/java/net/itsparkielad/medievalorigins/condition/ModConditions.java
+++ src/main/java/net/itsparkielad/medievalorigins/condition/ModConditions.java
@@ -6,18 +6,19 @@ import io.github.edwinmindcraft.apoli.api.registry.ApoliRegistries;
 import io.github.edwinmindcraft.apoli.common.condition.block.SimpleBlockCondition;
 import io.github.edwinmindcraft.apoli.common.condition.entity.SimpleEntityCondition;
 import net.itsparkielad.medievalorigins.MedievalOrigins;
-import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobType;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
+
 public class ModConditions {
     public static final DeferredRegister<EntityCondition<?>> ENTITY_CONDITIONS = DeferredRegister.create(ApoliRegistries.ENTITY_CONDITION_KEY, MedievalOrigins.MODID);
-    public static final RegistryObject<SimpleEntityCondition> FRIENDLY_TO = ENTITY_CONDITIONS.register("friendly_to", () ->
-            new SimpleEntityCondition(entity -> entity instanceof Animal));
-
+    public static final RegistryObject<SimpleEntityCondition> IS_UNDEAD = ENTITY_CONDITIONS.register("is_undead", () ->
+            new SimpleEntityCondition(entity -> entity instanceof LivingEntity && ((LivingEntity) entity).getMobType() == MobType.UNDEAD));
 
     public static final DeferredRegister<BlockCondition<?>> BLOCK_CONDITIONS = DeferredRegister.create(ApoliRegistries.BLOCK_CONDITION_KEY, MedievalOrigins.MODID);
     public static final RegistryObject<SimpleBlockCondition> HARVESTABLE_CROPS = BLOCK_CONDITIONS.register("harvestable_crops", () ->
