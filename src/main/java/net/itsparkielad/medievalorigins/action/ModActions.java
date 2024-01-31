e710d3730c4f95b1da007434d3a8995169e1eccf
muon-rw <ravenmenville@gmail.com>
2024-01-30 23:46:04 -0600

    I can't fucking believe this worked on the first try

--- src/main/java/net/itsparkielad/medievalorigins/action/ModActions.java
+++ src/main/java/net/itsparkielad/medievalorigins/action/ModActions.java
@@ -1,6 +1,8 @@
 package net.itsparkielad.medievalorigins.action;
 import io.github.edwinmindcraft.apoli.api.power.factory.*;
 import io.github.edwinmindcraft.apoli.api.registry.ApoliRegistries;
+import io.github.edwinmindcraft.apoli.common.action.bientity.SimpleBiEntityAction;
+import io.github.edwinmindcraft.apoli.common.registry.ApoliRegisters;
 import net.itsparkielad.medievalorigins.MedievalOrigins;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -19,7 +21,13 @@ public class ModActions {
     public static final RegistryObject<SummonWitherSkeletonAction> SUMMON_WITHER_SKELETON = ENTITY_ACTIONS.register("summon_wither_skeleton", SummonWitherSkeletonAction::new);
     public static final RegistryObject<SummonZombieAction> SUMMON_ZOMBIE = ENTITY_ACTIONS.register("summon_zombie", SummonZombieAction::new);
 
+
+    public static final DeferredRegister<BiEntityAction<?>> BIENTITY_ACTIONS = DeferredRegister.create(ApoliRegistries.BIENTITY_ACTION_KEY, MedievalOrigins.MODID);
+    public static final RegistryObject<TransferItemAction> TRANSFER_ITEM = BIENTITY_ACTIONS.register("transfer_item", () -> {
+        return new TransferItemAction(TransferItemAction::transferItem);
+    });
     public static void register(IEventBus eventBus) {
         ENTITY_ACTIONS.register(eventBus);
+        BIENTITY_ACTIONS.register(eventBus);
     }
 }
---
+++ src/main/java/net/itsparkielad/medievalorigins/action/TransferItemAction.java
@@ -0,0 +1,48 @@
+package net.itsparkielad.medievalorigins.action;
+
+import io.github.edwinmindcraft.apoli.api.configuration.NoConfiguration;
+import io.github.edwinmindcraft.apoli.api.power.factory.BiEntityAction;
+import net.itsparkielad.medievalorigins.entity.SummonedSkeleton;
+import net.itsparkielad.medievalorigins.entity.SummonedWitherSkeleton;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+
+
+import java.util.function.BiConsumer;
+
+
+public class TransferItemAction extends BiEntityAction<NoConfiguration> {
+
+    public static void transferItem(Entity actor, Entity target) {
+        if (actor instanceof LivingEntity livingActor && target instanceof LivingEntity livingTarget) {
+            ItemStack actorItem = livingActor.getMainHandItem().copy();
+            ItemStack targetItem = livingTarget.getMainHandItem().copy();
+
+            if (!actorItem.isEmpty() || !targetItem.isEmpty()) {
+                if (target instanceof SummonedSkeleton summon) {
+                    summon.setWeapon(actorItem.isEmpty() ? ItemStack.EMPTY : actorItem);
+                } else if (target instanceof SummonedWitherSkeleton summon) {
+                    summon.setWeapon(actorItem.isEmpty() ? ItemStack.EMPTY : actorItem);
+                } else {
+                    livingTarget.setItemInHand(InteractionHand.MAIN_HAND, actorItem.isEmpty() ? ItemStack.EMPTY : actorItem);
+                }
+                livingActor.setItemInHand(InteractionHand.MAIN_HAND, targetItem.isEmpty() ? ItemStack.EMPTY : targetItem);
+            }
+        }
+    }
+
+    private final BiConsumer<Entity, Entity> action;
+
+    public TransferItemAction(BiConsumer<Entity, Entity> action) {
+        super(NoConfiguration.CODEC);
+        this.action = action;
+    }
+
+    @Override
+    public void execute(NoConfiguration configuration, Entity actor, Entity target) {
+        this.action.accept(actor, target);
+    }
+
+}
