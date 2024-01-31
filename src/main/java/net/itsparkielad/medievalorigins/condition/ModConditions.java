0047bdec3b341c5c890c08c18dcadd0f00b9a11c
muon-rw <ravenmenville@gmail.com>
2024-01-31 13:21:24 -0600

    turns out they don't know how to use crossbows

--- src/main/java/net/itsparkielad/medievalorigins/condition/ModConditions.java
+++ src/main/java/net/itsparkielad/medievalorigins/condition/ModConditions.java
@@ -3,13 +3,20 @@ package net.itsparkielad.medievalorigins.condition;
 import io.github.edwinmindcraft.apoli.api.power.factory.BlockCondition;
 import io.github.edwinmindcraft.apoli.api.power.factory.DamageCondition;
 import io.github.edwinmindcraft.apoli.api.power.factory.EntityCondition;
+import io.github.edwinmindcraft.apoli.api.power.factory.ItemCondition;
 import io.github.edwinmindcraft.apoli.api.registry.ApoliRegistries;
 import io.github.edwinmindcraft.apoli.common.condition.block.SimpleBlockCondition;
 import io.github.edwinmindcraft.apoli.common.condition.damage.InTagCondition;
 import io.github.edwinmindcraft.apoli.common.condition.entity.SimpleEntityCondition;
+import io.github.edwinmindcraft.apoli.common.condition.item.SimpleItemCondition;
 import net.itsparkielad.medievalorigins.MedievalOrigins;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobType;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.DiggerItem;
+import net.minecraft.world.item.ProjectileWeaponItem;
+import net.minecraft.world.item.TridentItem;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,6 +29,17 @@ public class ModConditions {
     public static final RegistryObject<SimpleEntityCondition> IS_UNDEAD = ENTITY_CONDITIONS.register("is_undead", () ->
             new SimpleEntityCondition(entity -> entity instanceof LivingEntity && ((LivingEntity) entity).getMobType() == MobType.UNDEAD));
 
+
+    public static final DeferredRegister<ItemCondition<?>> ITEM_CONDITIONS = DeferredRegister.create(ApoliRegistries.ITEM_CONDITION_KEY, MedievalOrigins.MODID);
+    public static final RegistryObject<SimpleItemCondition> IS_WEAPON = ITEM_CONDITIONS.register("is_weapon", () ->
+            new SimpleItemCondition(stack ->
+                    Enchantments.SHARPNESS.canEnchant(stack) || stack.getItem() instanceof BowItem)
+            );
+    public static final RegistryObject<SimpleItemCondition> IS_TOOL = ITEM_CONDITIONS.register("is_tool", () ->
+            new SimpleItemCondition(stack ->
+                    Enchantments.BLOCK_EFFICIENCY.canEnchant(stack) || stack.getItem() instanceof DiggerItem)
+    );
+
     public static final DeferredRegister<BlockCondition<?>> BLOCK_CONDITIONS = DeferredRegister.create(ApoliRegistries.BLOCK_CONDITION_KEY, MedievalOrigins.MODID);
     public static final RegistryObject<SimpleBlockCondition> HARVESTABLE_CROPS = BLOCK_CONDITIONS.register("harvestable_crops", () ->
             new SimpleBlockCondition((level, pos, stateSuppplier) -> {
@@ -29,11 +47,13 @@ public class ModConditions {
                 if(state.getBlock() instanceof CropBlock crop) return crop.isMaxAge(state);
                 return false;
             }));
+
     public static final DeferredRegister<DamageCondition<?>> DAMAGE_CONDITIONS = DeferredRegister.create(ApoliRegistries.DAMAGE_CONDITION_KEY, MedievalOrigins.MODID);
     public static final RegistryObject<DamageCondition> IN_TAG = DAMAGE_CONDITIONS.register("in_tag", InTagCondition::new);
     public static void register(IEventBus eventBus) {
         ENTITY_CONDITIONS.register(eventBus);
         BLOCK_CONDITIONS.register(eventBus);
         DAMAGE_CONDITIONS.register(eventBus);
+        ITEM_CONDITIONS.register(eventBus);
     }
 }
